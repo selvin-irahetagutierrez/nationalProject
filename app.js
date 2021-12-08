efe7a9f98867c1beb0820ec3b6628a3f49f5d685
selvin Iraheta-Gutierrez <irahetaselvin9@gmail.com>
2021-12-07 22:20:14 -0500

    Final Edits From Notes

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ function app(people){
       searchResults = searchByName(people);
       break;
     case 'no':
-      // TODO: search by traits
-      searchByTraits(people);
+      searchByTrait(people)
       break;
       default:
     app(people); // restart app
@@ -21,7 +20,7 @@ function app(people){
   }
   
   // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
-  mainMenu(searchResults, people);
+  mainMenu(searchResults[0], people);
 }
 
 // Menu function to call once you find who you are looking for
@@ -38,21 +37,28 @@ function mainMenu(person, people){
 
   switch(displayOption){
     case "info":
-    // TODO: get person's info
     displayPerson(person);
-    mainMenu(person,people);
+    mainMenu(person, people);
     break;
+
     case "family":
-      displayFamilyInfo(person,people);
-      mainMenu(person,people);
-      break;
-    case "descendants":
-    displayPeople(displayDescendants(person,people));
-    mainMenu(person,people);
+    getSpouse(person, people);
+    getParents(person, people);
+    let sibilings = getSiblings(person, people);
+    if(sibilings.length !== 0 ? displaySibilings(sibilings) : alert(`${person.firstName} has no siblings`)); 
+    mainMenu(person, people);
     break;
+
+    case "descendants":
+    let parent_descendants = getDescendants(person, people);
+    if (parent_descendants.length !== 0 ? displayPeople(parent_descendants) : alert(`${person.firstName} has no descendants`));
+    mainMenu(person, people);
+    break;
+
     case "restart":
     app(people); // restart
     break;
+    
     case "quit":
     return; // stop execution
     default:
@@ -72,48 +78,276 @@ function searchByName(people){
       return false;
     }
   })
-  // TODO: find the person using the name they entered
-  return foundPerson[0];
+
+  return foundPerson;
 }
 
-function selectTrait(){
-  let trait = promptFor("Which Trait would you like to search by?\n Gender, Height, Weight, Eye Color, Occupation, None", chars)
-  return trait;
-}
-
-
-function searchByTraits(people) {
-
-
-}
-let display;
-  let searchAgain = true;
+// Used with displayDecendants 
+function getDescendants(person, people, children=[], idx = 0) {
   
+  let parent_tree = people.filter((item) => {
+    if(item.parents.includes(person.id)) {
+      children.push(item);
+      return true;
+    }
+  });
 
-  while(testing){
-    let trait = selectTrait().toLowerCase();
-  switch (trait) {
-    case "gender":
-      display = searchGender(people);
-      displayPeople(display);
-      break;
-    case "height":
-      display = searchHeight(people);
-      displayPeople(display);
-      break;
-    case "weight":
-      display = searchWeight(people);
-      displayPeople(display);
-      break;
-    case "eye color":
-      display = searchEyeColor(people);
-      displayPeople(display);
-      break;
-    case "occupation":
-      display = searchOccupation(people);
-      displayPeople(display);
-      break;
-    default:
-      break;
+  for (let i = idx; i < children.length; i++) { 
+    return getDescendants(children[i], people, children, i + 1)
   }
+
+  return children;
+}
+
+function getSiblings(person, people) {
+  let siblings = people.filter((item) => {
+    for(let i = 0; i < person.parents.length; i++) {
+      if(item.parents.includes(person.parents[i])){
+        return true;
+      }
+    }
+  })
+  return siblings;
+}
+
+
+function getSpouse(person, people) {
+
+  let spouse = people.filter((item) => {
+    if (person.currentSpouse === item.id) {
+      return true;
+    }
+  })
+  if (spouse.length === 0) {
+    alert(`${person.firstName} is not married.`)
+  } else {
+      alert(`${person.firstName} is married to ${spouse[0]['firstName']} ${spouse[0]['lastName']}`);
+  }
+  return spouse;
+}
+
+function getParents(person, people) {
+  let parents = [];
+
+  let parent = people.filter((item) => {
+    if(person.parents.includes(item.id)) {
+      parents.push(item)
+      return true;
+    } 
+  })
+   
+  if (parents.length === 0){
+    alert(`${person.firstName} has no parents`)
+  }else {
+    let family = '';
+    for (let i = 0; i < parents.length; i++) {
+      family += `${parents[i].firstName} ${parents[i].lastName}`
+    }
+    alert(`Parents: ${family}`)
+  }
+  
+}
+
+// alerts a list of people
+function displayPeople(people){
+  alert(people.map(function(person){
+    return person.firstName + " " + person.lastName + "\n";
+  }).join("\n"));
+}
+
+function displaySibilings(people) {
+  alert(people.map(function(person){
+    return `Sibling: ${person.firstName} ${person.lastName} \n`;
+  }).join("\n"));
+}
+
+function displayPerson(person){
+  // print all of the information about a person:
+  // height, weight, age, name, occupation, eye color.
+
+  let personInfo = 'First Name: ' + person.firstName + '\n' 
+  personInfo+= 'Last Name: ' + person.lastName + '\n'
+  personInfo += 'Gender: ' + person.gender + '\n' 
+  personInfo += 'DOB: ' + person.dob + '\n' 
+  personInfo += 'Height: ' + person.height + '\n'
+  personInfo += 'Weight: ' + person.weight + '\n' 
+  personInfo += 'EyeColor: ' + person.eyeColor + '\n'
+  personInfo += 'Occupation: ' + person.occupation
+  alert(personInfo);
+}
+
+// function that prompts and validates user input
+function promptFor(question, valid){
+  do{
+    var response = prompt(question).trim();
+  } while(!response || !valid(response));
+  return response;
+}
+
+// helper function to pass into promptFor to validate yes/no answers
+function yesNo(input){
+  return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
+}
+
+// helper function to pass in as default promptFor validation
+function chars(input){
+  return true; // default validation only
+}
+
+//Gender validation
+function validateGender(input){ 
+  return input.toLowerCase() == "male" || input.toLowerCase() == "female";
+}
+
+//Integer validation 
+function integers(input){ 
+  if(parseInt(input)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+//Function to search by single trait
+function searchByTrait(people){    
+  let input = promptFor("Which trait would you like to search by?(Use the Numbers) \n1: Gender\n2: Height\n3: Weight\n4: Eye Color\n5: Occupation\n6: DOB\n7 Restart Application?",integers);
+  let foundPeople = [];
+  switch(input){
+    case"1"://gender           
+        foundPeople = genderSearch(people);     
+      break;
+    case "2"://height      
+        foundPeople = getHeight(people);      
+      break;
+    case "3"://weight     
+        foundPeople = getWeight(people);      
+      break;
+    case "4"://eye color      
+        foundPeople = getEyes(people);      
+        break;
+    case "5"://occupation      
+        foundPeople = getOccupation(people);      
+        break;
+    case "6"://DOB      
+        foundPeople = getAge(people);      
+        break;
+    case "7"://restart
+        app(people); 
+        break;
+    default:
+      alert("Enter valid selection.");
+      return searchByTrait(people);
+  }
+    if (foundPeople.length > 1) {
+      displayPeople(foundPeople);
+      searchByTrait(foundPeople)
+    } else if (foundPeople.length === 1) {
+      let foundPerson = foundPeople[0];
+      mainMenu(foundPerson, people);
+    } else {
+      app(people);
+    }
+}
+
+//Function to prompt no results if criteria doesnt match. 
+function noResults(){
+  alert("Could not find a trait match!");
+}
+
+//search function for gender
+function genderSearch(people){ 
+  let userChoice = promptFor("Is the person male or female?", validateGender);
+  let foundPeople = people.filter(function(el){
+    if(el.gender === userChoice) {      
+      return true;
+    }});
+    if(foundPeople === undefined|| foundPeople.length === 0){
+      noResults();
+      return app(people);
+    }
+   return foundPeople;
+}
+
+//search function for height
+function getHeight(people){  
+  let userChoice = promptFor("What is the person's height in inches?",integers);
+  let foundPeople = people.filter(function(el){
+    if(el.height == userChoice){
+      return true;
+    }});
+  if(foundPeople=== undefined || foundPeople.length ===0){
+    noResults();
+    return app(people);
+  }
+  return foundPeople;
+}
+
+//search function for weight
+function getWeight(people){ 
+  let userChoice = promptFor("What is the person's weight?",integers);
+  let foundPeople = people.filter(function(el){
+    if(el.weight == userChoice){
+      return true;
+    }});
+  if(foundPeople=== undefined || foundPeople.length ===0){
+    noResults();
+    return app(people);
+  }
+  return foundPeople;
+}
+
+//search function for eye color
+function getEyes(people){
+  let userChoice = promptFor("What is the person's eye color?",chars);
+  let foundPeople = people.filter(function(el){
+    if(el.eyeColor == userChoice){
+      return true;
+    }});
+  if(foundPeople=== undefined || foundPeople.length ===0){
+    noResults();
+    return app(people);
+  }
+  return foundPeople;
+}
+
+//search function for occupation 
+function getOccupation(people){
+  let userChoice = promptFor("What is the person's occupation?",chars);
+  let foundPeople = people.filter(function(el){
+    if(el.occupation == userChoice){
+      return true;
+    }});
+  if(foundPeople=== undefined || foundPeople.length ===0){
+    noResults();
+    return app(people);
+  }
+  return foundPeople;
+}
+
+//search function for age
+function getAge(people){
+  let userChoice = promptFor("What is the person's date of birth? \n Format: MM/DD/YYY",integers);
+    userChoice = ageFinder(userChoice);
+  let foundPeople = people.filter(function(el){
+    if(ageFinder(el.dob) == userChoice){
+      return true;
+    }});
+  if(foundPeople=== undefined || foundPeople.length ===0){
+    noResults();
+    return app(people);
+  }
+  return foundPeople;
+}
+
+//function that converts DOB into age integer.
+function ageFinder(dob){
+  var today = new Date();
+  var birthDate = new Date(dob);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())){
+    age--;
+  }
+  return age;
 }
